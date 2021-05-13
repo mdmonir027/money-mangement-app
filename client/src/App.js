adf22e8a619837a226683a273585afc6dc1bcae2
Md Monirul Islam <mmislam027@gmail.com>
2021-05-13 23:27:44 +0600

    Frontend Setup with react router and redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h3> Hello world</h3>
+        <Router>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+          </Switch>
+          <Link to='/'>Home</Link>
+          <Link to='/login'>Login</Link>
+          <Link to='/register'>Register</Link>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h3> Login</h3>
+      </div>
+    );
+  }
+}
+
+export default Login;
